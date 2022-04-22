a46d1cdf4787dab981a1aef001cbb19a3677fc43
SashaGurin <top-on3@mai.ru>
2022-04-22 13:37:23 +0300

    + mobile_nav

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/* ================= MOBILE NAV ========================= */
+const mobileNavButton = document.querySelector(".mobile-nav-button");
+const mobileNavIcon = document.querySelector(".mobile-nav-button__icon");
+const mobileNav = document.querySelector(".mobile__nav");
+
+mobileNavButton.addEventListener("click", function () {
+  mobileNavIcon.classList.toggle("active");
+  mobileNav.classList.toggle("active");
+  document.body.classList.toggle("no-scroll");
+});
